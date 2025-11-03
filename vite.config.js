a07a94ea05f5adb463313587095fee9ff41ab3f3
Anant-Navadiya <anantnavadiya1530@gmail.com>
2025-11-03 12:26:26 +0530

    updated:alias

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@/images': path.resolve(__dirname, './public/images'),
+            '@': path.resolve(__dirname, './public'),
         },
     },
 });
